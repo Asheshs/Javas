277bcd149af75b826650c2f006f0225f9c5da979
ashesh <asheshfootball@gmail.com>
2018-12-21 08:08:33 +0545

    changed name in PersonalInformation file

--- PersonalInformation.java
+++ PersonalInformation.java
@@ -3,7 +3,7 @@ class PersonalInformation
     public static void main(String[]args)
     {
         System.out.println("\t personal information ");
-        System.out.println("\n name:ashesh");
+        System.out.println("\n name:ashesi");
         System.out.println("\n phone:9846908435");
         System.out.println("\n email:asheshfootball@gmail.com");
     }
